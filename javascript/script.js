fc78ec7483204f3a27299176e0bbe986abdde426
Kubilay Birer <kubilaybirer@hotmail.com>
2023-09-10 13:13:11 +0300

    javascript variable types

--- javascript/script.js
+++ javascript/script.js
@@ -7,36 +7,89 @@
 
 // Variables
 
-var password;
-console.log(password)
+// var password;
+// console.log(password)
 
-password = "kubilaybrr"
-console.log(password)
+// password = "kubilaybrr"
+// console.log(password)
 
-age = 22;
+// age = 22;
+// console.log(age)
+
+// // variable declaring rules
+
+
+// // cannot start with numbers
+
+// age1=31;
+
+// // we cant use command names as variable names
+
+// var if1;
+
+// // we cant use spacing in variables
+// // instead we use camelcase or underline or snakecase
+// // we shouldnt use special characters for ex turkish letters like ü,i,ş,ö..
+// var ad_soyad;
+// var adSoyad;
+// adSoyad="Kubilay Birer"
+// console.log(adSoyad)
+
+// const email= "12345@gmail.com";
+// console.log(email)
+
+// email = "56789@gmail.com"
+// console.log(email)
+
+// Variable Types
+
+// Primitive Types
+
+// String
+
+let firstName="Kubi";
+console.log(firstName)
+console.log(typeof(firstName))
+
+// Number
+
+let age = 31;
 console.log(age)
+console.log(typeof(age))
 
-// variable declaring rules
+// Boolean (logical)
 
+let credit_isused=false
+console.log(credit_isused)
+console.log(typeof(credit_isused))
 
-// cannot start with numbers
+// Undefined 
 
-age1=31;
+let phone;
+console.log(phone)
+console.log(typeof(phone))
 
-// we cant use command names as variable names
+// Reference Types : Objects
 
-var if1;
+    //Array
 
-// we cant use spacing in variables
-// instead we use camelcase or underline or snakecase
-// we shouldnt use special characters for ex turkish letters like ü,i,ş,ö..
-var ad_soyad;
-var adSoyad;
-adSoyad="Kubilay Birer"
-console.log(adSoyad)
+    let list = ["Aslı","Serker","Keroş"]
+    console.log(list)
+    console.log(typeof(list))
 
-const email= "12345@gmail.com";
-console.log(email)
+    //Object Literals
 
-email = "56789@gmail.com"
-console.log(email)
+    let adress = {
+        city:"İstanbul",
+        country:"Türkiye",
+    }
+    console.log(adress)
+    console.log(typeof(adress))
+
+// Funcitons
+
+var calculate = function(){
+    return 10;
+}
+console.log(calculate)
+console.log(typeof(calculate))
