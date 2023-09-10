8c5e2d1e7977796b86209ac4a3dd955dd7bb62ce
Kubilay Birer <kubilaybirer@hotmail.com>
2023-09-10 13:03:24 +0300

    js variable declaring essentials

--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,37 @@
 // console.clear()
 
 // Variables
+
+var password;
+console.log(password)
+
+password = "kubilaybrr"
+console.log(password)
+
+age = 22;
+console.log(age)
+
+// variable declaring rules
+
+
+// cannot start with numbers
+
+age1=31;
+
+// we cant use command names as variable names
+
+var if1;
+
+// we cant use spacing in variables
+// instead we use camelcase or underline or snakecase
+// we shouldnt use special characters for ex turkish letters like ü,i,ş,ö..
+var ad_soyad;
+var adSoyad;
+adSoyad="Kubilay Birer"
+console.log(adSoyad)
+
+const email= "12345@gmail.com";
+console.log(email)
+
+email = "56789@gmail.com"
+console.log(email)
