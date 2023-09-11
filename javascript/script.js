bacb57471c15aa6291f7a663c950c32576920e92
Kubilay Birer <kubilaybirer@hotmail.com>
2023-09-11 13:39:16 +0300

    worked on javascript date and time

--- javascript/script.js
+++ javascript/script.js
@@ -47,98 +47,121 @@
 
 // String
 
-let firstName="Kubi";
-console.log(firstName)
-console.log(typeof(firstName))
+// let firstName="Kubi";
+// console.log(firstName)
+// console.log(typeof(firstName))
 
-// Number
+// // Number
 
-let age = 31;
-console.log(age)
-console.log(typeof(age))
+// let age = 31;
+// console.log(age)
+// console.log(typeof(age))
 
-// Boolean (logical)
+// // Boolean (logical)
 
-let credit_isused=false
-console.log(credit_isused)
-console.log(typeof(credit_isused))
+// let credit_isused=false
+// console.log(credit_isused)
+// console.log(typeof(credit_isused))
 
-// Undefined 
+// // Undefined 
 
-let phone;
-console.log(phone)
-console.log(typeof(phone))
+// let phone;
+// console.log(phone)
+// console.log(typeof(phone))
 
-// Reference Types : Objects
+// // Reference Types : Objects
 
-    //Array
+//     //Array
 
-    let list = ["Aslı","Serker","Keroş"]
-    console.log(list)
-    console.log(typeof(list))
+//     let list = ["Aslı","Serker","Keroş"]
+//     console.log(list)
+//     console.log(typeof(list))
 
-    //Object Literals
+//     //Object Literals
 
-    let adress = {
-        city:"İstanbul",
-        country:"Türkiye",
-    }
-    console.log(adress)
-    console.log(typeof(adress))
+//     let adress = {
+//         city:"İstanbul",
+//         country:"Türkiye",
+//     }
+//     console.log(adress)
+//     console.log(typeof(adress))
 
-// Funcitons
+// // Funcitons
 
-var calculate = function(){
-    return 10;
-}
-console.log(calculate)
-console.log(typeof(calculate))
+// var calculate = function(){
+//     return 10;
+// }
+// console.log(calculate)
+// console.log(typeof(calculate))
 
-// Mathematical operators
-//a20 b10 c5 d3
-let data;
-const a=20;
-const b=10;
-const c=5;
-let d=3;
+// // Mathematical operators
+// //a20 b10 c5 d3
+// let data;
+// const a=20;
+// const b=10;
+// const c=5;
+// let d=3;
 
-data = a+b;
-data=a-b;
-data=a*b;
-data=a/b;
+// data = a+b;
+// data=a-b;
+// data=a*b;
+// data=a/b;
 
-// Assignment operators
+// // Assignment operators
 
-data=a;
-data+=a; // data = data+a
-data-=a; // data = data-a
-data*=a; // data = data*a
-data/=a; // data = data/a
-data%=a; // data = data%a
+// data=a;
+// data+=a; // data = data+a
+// data-=a; // data = data-a
+// data*=a; // data = data*a
+// data/=a; // data = data/a
+// data%=a; // data = data%a
 
-// Comparison operators
-//a20 b10 c5 d3
+// // Comparison operators
+// //a20 b10 c5 d3
 
-data = a==b;  // are they equal
-data = b==c;  // are they equal
-data = 5=="5";  // are they equal yes because they are same
-data = 5==="5";  // are they equal no because types aren't the same
-data = a!=b;  // a is not equal to b is true
-data = a!==b;  // a not is equal to b 
+// data = a==b;  // are they equal
+// data = b==c;  // are they equal
+// data = 5=="5";  // are they equal yes because they are same
+// data = 5==="5";  // are they equal no because types aren't the same
+// data = a!=b;  // a is not equal to b is true
+// data = a!==b;  // a not is equal to b 
 
-// Logical operators
-//a20 b10 c5 d3
+// // Logical operators
+// //a20 b10 c5 d3
 
-// && and
-data = (a>b) && (a>c)
-// || or
-data = (b>a) || (c>a)
-// ! not
-data=!(a<b)
+// // && and
+// data = (a>b) && (a>c)
+// // || or
+// data = (b>a) || (c>a)
+// // ! not
+// data=!(a<b)
+
+// console.log(data);
+// console.log(typeof(data));
+
+// DATE OBJECT
+
+let time = new Date();
+let birthday= new Date(2001,7,10);
+
+// Set Methods
+time.setDate(25);
+time.setMonth(3);
+// time.setFullYear(2013);
+
+// Get Methods
+console.log(time.getMonth()+1)
+console.log(time.getDate())
+console.log(time.getFullYear())
+console.log(time.getDay())
+console.log(time.getHours())
+console.log(time.getMinutes())
+console.log(time.getSeconds())
+console.log(time.getMilliseconds())
 
 
 
+console.log(time);
+console.log(typeof(time));
 
-
-console.log(data);
-console.log(typeof(data));
+console.log("your age=",time.getFullYear()-birthday.getFullYear());
