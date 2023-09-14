eecbd2a434709c6ba1a9f1419cdf888253a4bc52
Kubilay Birer <kubilaybirer@hotmail.com>
2023-09-14 13:41:59 +0300

    worked on switch case in javascript

--- javascript/script.js
+++ javascript/script.js
@@ -339,3 +339,65 @@
 // else{
 //     console.log("no id")
 // }
+
+// Switch koşul yapısı
+
+// let islem=4;
+
+// switch(islem){
+//     case 1:
+//         console.log("1 nolu işlem yapıldı")
+//         break;
+//     case 2:
+//         console.log("2 nolu işlem yapıldı")
+//         break;
+//     case 3:
+//         console.log("3 nolu işlem yapıldı")
+//         break;
+//     default:
+//         console.log("İşlem yapılmadı")
+    
+    
+// }
+
+// let day;
+
+// switch(new Date().getDay()){
+//     case 0:
+//         day="Pazar";
+//         break
+//     case 1:
+//         day="Pazartesi";
+//         break
+//     case 2:
+//         day="Salı";
+//         break
+//     case 3:
+//         day="Çarşamba";
+//         break
+//     case 4:
+//         day="Perşembe";
+//         break
+//     case 5:
+//         day="Cuma";
+//         break
+//     case 6:
+//         day="Cumartesi";
+//         break
+// }
+
+// console.log(day);
+
+let hour=8;
+
+switch(true){
+    case (hour >=6 && hour<=12):
+        console.log("Günaydın");
+        break
+    case (hour >=13 && hour<=17):
+        console.log("İyi Günler");
+        break
+    case (hour >=18 && hour<=24):
+        console.log("İyi Akşamlar");
+        break
+}
