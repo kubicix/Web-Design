72475a27a2f995ee68df755c06ee8ba5a425a68c
Kubilay Birer <kubilaybirer@hotmail.com>
2023-09-14 13:33:39 +0300

    worked on if else in javascript

--- javascript/script.js
+++ javascript/script.js
@@ -292,3 +292,50 @@
 
 // names.splice(2,1,"pkero");
 // console.log(names);
+
+// IF-ELSE conditions
+
+// const firstName="Çağla";
+// const userName="caglayilmaz";
+// const age=20;
+// const isStudent=true;
+// const school="university";
+
+// if(userName == "caglayilmaz"){
+//     console.log("Merhaba Çağla");
+// }
+// else{
+//     console.log("Kullanıcı bulunamadı");
+// }
+
+// if(age === 20){
+//     console.log("Yaşınız 20");
+// }
+
+// if(isStudent){
+//     console.log("Hangi bölümde okuyorsunuz?");
+// }
+// else{
+//     console.log("Mesleğiniz nedir?");
+// }
+
+// if(age >= 18){
+//     if(school=="university"){
+//         console.log("Ehliyet alabilirsiniz");
+        
+//     }
+//     else{
+//         console.log("Eğitim durumu yetersiz alamazsınız");
+//     }
+// }
+// else{
+//     console.log("Yaşınız ehliyet için yetersiz");
+// }
+
+// let id="23232322"
+// if(typeof id != "undefined"){
+//     console.log("id: "+ id);
+// }
+// else{
+//     console.log("no id")
+// }
