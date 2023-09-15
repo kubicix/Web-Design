90886821e6ae2b21f4adaf9e3f1a29086b5353e7
Kubilay Birer <kubilaybirer@hotmail.com>
2023-09-15 13:10:18 +0300

    used loops on arrays and objects

--- javascript/script.js
+++ javascript/script.js
@@ -517,15 +517,50 @@
 
 let citys=["istanbul","Kocaeli","İzmir","Eskişehir","Edirne"];
 
+let users=[
+    {firstName:"Asya",lastName:"Mort"},
+    {firstName:"Kubi",lastName:"Grimes"},
+    {firstName:"Eren",lastName:"Yaegar"},
+    {firstName:"Fıstık",lastName:"Hammy"},
+]
+
 // Arrays
 
-for(let i =0;i<citys.length;i++){
-    console.log(citys[i]);
-}
+// for(let i =0;i<citys.length;i++){
+//     console.log(citys[i]);
+// }
 
 // for-in method
 
-for(let i in citys){
-    console.log(`${i}. ${citys[i]}`); // index number is printed out as well, we can use this way too but it
-}
+// for(let i in citys){
+//     console.log(`${i}. ${citys[i]}`); // index number is printed out as well, we can use this way too but it
+// }
 
+// for-each method
+
+// citys.forEach(function(item){
+//     console.log(`City name :${item}`)});
+
+// for(let i=0;i<users.length;i++){
+//     console.log(users[i].firstName+" "+users[i].lastName)
+// }
+
+// for(let i in users){
+//     console.log(`index ${i} value: ${users[i].firstName}`)
+// }
+
+// map:returns an array
+
+// let veri = users.map(function(item){
+//     return item.firstName+" "+item.lastName
+// })
+
+// console.log(veri);
+
+let numbers =[1,3,5,9,12];
+
+let num = numbers.map(function(n){
+    return n*n;
+})
+
+console.log(num)
