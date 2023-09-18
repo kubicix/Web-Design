794f78f1063a69fb13e122d96008d86db4ad234e
Kubilay Birer <kubilaybirer@hotmail.com>
2023-09-18 13:42:46 +0300

    worked on selecting elements in javascript

--- dom_document/script.js
+++ dom_document/script.js
@@ -53,7 +53,71 @@ let vei;
 // document.querySelector("li:last-child").style.color="red";
 // document.querySelector("li:nth-child(2)").style.color="green";
 
-document.querySelector("li").className="list-group-item list-group-item-danger";
+// document.querySelector("li").className="list-group-item list-group-item-danger";
 
-document.querySelector("li").classList.add("active");
+// document.querySelector("li").classList.add("active");
 
+// -- Selecting Multiple Elements --
+
+// document.getElementsByClassName()
+
+// let veri;
+
+// veri = document.getElementsByClassName("list-group-item")
+// veri = document.getElementsByClassName("list-group-item")[0]
+// veri = document.getElementsByClassName("list-group-item")[2]
+
+// veri=veri[2]
+// veri[1].style.fontSize="30px";
+// veri[1].style.color="red";
+// veri[2].textContent="new item";
+
+// for(let i =0;veri.length;i++){
+//     console.log(veri[i].style.color="orange")
+//     console.log(veri[i].textContent="new items")
+// }
+
+// document.getElementsByTagName()
+
+// veri = document.getElementsByTagName("li");
+// veri = document.getElementsByTagName("a");
+// veri = document.getElementById("task-list").getElementsByTagName("a");
+
+// document.querySelectorAll()
+
+// veri=document.querySelectorAll("li");
+
+// veri.forEach(function(item,index){
+//     item.textContent=`${index}-item`;
+// })
+
+// console.log(veri);
+
+// Travelling through items
+
+let value;
+
+const todoList=document.querySelector(".list-group");
+const todo=document.querySelector(".list-group-item:nth-child(2)");
+const card=document.querySelector(".card");
+
+
+value = todoList;
+value = todo;
+value = card;
+
+// Child Nodes
+
+//childNodes will get all the elements
+value = todoList.childNodes;
+//children will only get list items,elements
+value = todoList.children;
+value = todoList.children[0];
+value = todoList.children[todoList.children.length - 1];
+value = todoList.children[1].textContent="changed item";
+
+value = card;
+value = card.children;
+value = card.children[1].children[0].textContent="Greetings";
+
+console.log(value);
