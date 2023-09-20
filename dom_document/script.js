9cd6b35d9754a1ec9533d4f947796f8141bd94b7
Kubilay Birer <kubilaybirer@hotmail.com>
2023-09-20 12:47:58 +0300

    worked on updating elements in javascript

--- dom_document/script.js
+++ dom_document/script.js
@@ -190,3 +190,41 @@ let vei;
 // }
 
 // console.log(taskList);
+
+// Updating Elements
+
+const cardHeader = document.querySelector(".card-header");
+
+const h2 = document.createElement("h2");
+h2.setAttribute("class","card-header");
+h2.appendChild(document.createTextNode("New Lists"));
+
+const parent = document.querySelector(".card");
+parent.replaceChild(h2,cardHeader);
+
+// Updating elements on classes
+
+const taskList=document.querySelector("#task-list");
+
+let value;
+
+link = taskList.children[0].children[0];
+value=link.className;
+value=link.classList;
+// value=link.classList[0]
+// value=link.classList[1]
+
+link.classList.add("new");
+link.classList.remove("new");
+
+// Updating Attributes
+
+value=link.getAttribute("href");
+value=link.setAttribute("href","https://instagram.com/kubilaybirer");
+
+value=link.hasAttribute("href");
+
+
+
+
+console.log(value)
