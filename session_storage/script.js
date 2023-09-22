4978e7a7b332548f3e4a689b711ce5e313311b11
Kubilay Birer <kubilaybirer@hotmail.com>
2023-09-22 13:01:14 +0300

    worked on session storage with example web app

---
+++ session_storage/script.js
@@ -0,0 +1,32 @@
+
+// Tarayıcı Depolama Alanları
+
+
+// Session Storage
+
+const add= document.querySelector("#add");
+const del= document.querySelector("#delete");
+const clear= document.querySelector("#clear");
+
+const addkey=document.querySelector("#addkey");
+const addvalue=document.querySelector("#addvalue");
+const deletekey=document.querySelector("#deletekey");
+
+add.addEventListener("click",addItem);
+del.addEventListener("click",deleteItem);
+clear.addEventListener("click",clearItems);
+
+function addItem(e){
+    sessionStorage.setItem(addkey.value,addvalue.value);
+}
+
+function deleteItem(e){
+    sessionStorage.removeItem(deletekey.value);
+}
+
+function clearItems(e){
+    sessionStorage.clear()
+}
+
+
+// Local Storage
