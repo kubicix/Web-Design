c75a9894f4081773f970ab972a6d58028d4ea044
Kubilay Birer <kubilaybirer@hotmail.com>
2023-09-30 12:32:53 +0300

    worked on inheritance in js

--- oop/script.js
+++ oop/script.js
@@ -29,22 +29,22 @@
 
 //  Prototype 
 
-let Person = function(name,yearOfBirth,job){
-    this.name=name;
-    this.yearOfBirth=yearOfBirth;
-    this.job=job;
+// let Person = function(name,yearOfBirth,job){
+//     this.name=name;
+//     this.yearOfBirth=yearOfBirth;
+//     this.job=job;
     
-}
+// }
 
-let ayaz = new Person("ayaz",1980,"teacher");
+// let ayaz = new Person("ayaz",1980,"teacher");
 
-Person.prototype.calculateAge =function(){
-    return 2023-this.yearOfBirth;
-}
+// Person.prototype.calculateAge =function(){
+//     return 2023-this.yearOfBirth;
+// }
 
-Person.prototype.getName = function(){
-    return this.name;
-}
+// Person.prototype.getName = function(){
+//     return this.name;
+// }
         
 // console.log(ayaz.calculateAge())
 // console.log(ayaz);
@@ -52,26 +52,50 @@ Person.prototype.getName = function(){
 
 // Object.create
 
-let personProto= {
-    calculateAge : function(){
-        return 2023 - this.yearOfBirth
-    }
+// let personProto= {
+//     calculateAge : function(){
+//         return 2023 - this.yearOfBirth
+//     }
+// }
+
+// let kubi = Object.create(personProto)
+
+// kubi.name="kubi"
+// kubi.yearOfBirth=2001
+// kubi.job="student"
+
+// let asya =Object.create(personProto,{
+//     name:{value:"asya"},
+//     yearOfBirth:{value:2002},
+//     job:{value:"student"}
+// })
+
+// console.log(kubi)
+// console.log(kubi.calculateAge())
+// console.log("--------------")
+// console.log(asya)
+// console.log(asya.calculateAge())
+
+// Prototype based Inheritance
+
+let Person = function(name,yearOfBirth,job){
+        this.name=name;
+        this.yearOfBirth=yearOfBirth;
+        this.job=job;
 }
 
-let kubi = Object.create(personProto)
+Person.prototype.calculateAge=function(){
+    return  2023 - this.yearOfBirth ;
+}
 
-kubi.name="kubi"
-kubi.yearOfBirth=2001
-kubi.job="student"
+let Teacher = function(name,yearOfBirth,job,subject){
+    //Inheritance method
+    Person.call(this,name,yearOfBirth,job)
+    this.subject=subject;
+}
 
-let asya =Object.create(personProto,{
-    name:{value:"asya"},
-    yearOfBirth:{value:2002},
-    job:{value:"student"}
-})
+Teacher.prototype=Object.create(Person.prototype)
 
-console.log(kubi)
-console.log(kubi.calculateAge())
-console.log("--------------")
-console.log(asya)
-console.log(asya.calculateAge())
+let aylin = new Teacher("aylin",1980,"teacher","history")
+console.log(aylin)
+console.log(aylin.calculateAge())
